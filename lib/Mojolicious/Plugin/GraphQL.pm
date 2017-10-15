b5f4170cc0deeabc118a0fcd37eefeae951d58f1
Ed J <mohawk2@users.noreply.github.com>
2017-10-16 00:23:29 +0100

    v0.02

--- lib/Mojolicious/Plugin/GraphQL.pm
+++ lib/Mojolicious/Plugin/GraphQL.pm
@@ -6,7 +6,7 @@ use Mojo::Base 'Mojolicious::Plugin';
 use Mojo::JSON qw(decode_json to_json);
 use GraphQL::Execution qw(execute);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my @DEFAULT_METHODS = qw(get post);
 my $EXECUTE = sub {

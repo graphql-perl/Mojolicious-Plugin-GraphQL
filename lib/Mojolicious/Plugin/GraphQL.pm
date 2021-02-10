a9af0a6fcd3af4fc70a66664388b8858deda9cce
Ed J <mohawk2@users.noreply.github.com>
2021-02-10 19:40:39 +0000

    0.19

--- lib/Mojolicious/Plugin/GraphQL.pm
+++ lib/Mojolicious/Plugin/GraphQL.pm
@@ -12,7 +12,7 @@ use Mojo::Promise;
 use curry;
 use Exporter 'import';
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 our @EXPORT_OK = qw(promise_code);
 
 use constant DEBUG => $ENV{GRAPHQL_DEBUG};
